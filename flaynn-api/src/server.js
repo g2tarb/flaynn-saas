20e6eca914a4f03eb49491e765386521bc96cb3a
Erwin Yana <erwinny82@gmail.com>
2026-04-04 10:40:02 +0200

    Update server.js

--- flaynn-api/src/server.js
+++ flaynn-api/src/server.js
@@ -153,8 +153,8 @@ export const start = async () => {
     });
 
     // Gestion du Dashboard SPA
-    fastify.get('/dashboard', async (_request, reply) => reply.redirect(302, '/dashboard/'));
-    fastify.get('/auth', async (_request, reply) => reply.redirect(302, '/auth/'));
+    fastify.get('/dashboard', async (_request, reply) => reply.code(302).redirect('/dashboard/'));
+    fastify.get('/auth', async (_request, reply) => reply.code(302).redirect('/auth/'));
 
     fastify.setNotFoundHandler(async (request, reply) => {
       if (request.method !== 'GET') return reply.code(404).send({ error: 'Not Found' });
